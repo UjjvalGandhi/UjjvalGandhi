4b634edf7811cb92c38c56b8708bb47589a18726
UjjvalGandhi <141738049+UjjvalGandhi@users.noreply.github.com>
2023-08-28 13:43:54 +0530

    Add files via upload
    this code is about push button and count that how many time it push by user

---
+++ Pushme.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: Scaffold(body: Center(child: Counter(),),),));
+
+class Counter extends StatefulWidget{
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter>{
+  int _counter = 0;
+
+  void _increment(){
+    setState(() {
+      _counter++;
+    });
+  }
+  @override
+  Widget build(BuildContext context){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextButton(
+            onPressed: _increment,
+            child: Text('Pushed me')),
+        Text('You pressed the button $_counter times'),
+      ],
+    );
+  }
+}
